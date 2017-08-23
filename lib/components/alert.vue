cryptodev1990/bootstrap-vue

commit 61fd80d503dc9723b21158e144a253aaa6497a98

fix(alert): Fix auto-dimissing alert "bug" (#897)
* fix(alert): Fix edgecase where dismissable alert couldnt be re-shown

* Update README.md

* [alert] add slot for dimiss button content

* Update meta.json

* [alert] remove deprecated state prop

lib/components/alert.vue
@@ -9,10 +9,9 @@
                 class="close"
                 data-dismiss="alert"
                 :aria-label="dismissLabel"
-                v-if="localDismissible"
                 @click.stop.prevent="dismiss"
         >
-            <span aria-hidden="true">&times;</span>
+            <span class="d-inline-block" aria-hidden="true"><slot name="dismiss">&times;</slot></span>
         </button>
         <slot></slot>
     </div>
@@ -22,24 +21,22 @@
     import {warn} from '../utils';
 
     export default {
+        model: {
+            prop: 'show',
+            event: 'input'
+        },
         data() {
             return {
                 countDownTimerId: null,
-                dismissed: false,
-                localDismissible: this.dismissible
+                dismissed: false
             };
-        },
-        created() {
-            if (this.state) {
-                warn('<b-alert> "state" property is deprecated, please use "variant" property instead.');
-            }
         },
         computed: {
             classObject() {
-                return ['alert', this.alertVariant, this.localDismissible ? 'alert-dismissible' : ''];
+                return ['alert', this.alertVariant, this.dismissible ? 'alert-dismissible' : ''];
             },
             alertVariant() {
-                const variant = this.state || this.variant || 'info';
+                const variant = this.variant;
                 return `alert-${variant}`;
             },
             localShow() {
@@ -50,10 +47,6 @@
             variant: {
                 type: String,
                 default: 'info'
-            },
-            state: {
-                type: String,
-                default: null
             },
             dismissible: {
                 type: Boolean,
@@ -81,9 +74,17 @@
         },
         methods: {
             dismiss() {
+                this.clearCounter();
                 this.dismissed = true;
                 this.$emit('dismissed');
-                this.clearCounter();
+                this.$emit('input', false);
+                if (typeof this.show === 'number') {
+                    this.$emit('dismiss-count-down', 0);
+                    this.$emit('input', 0);
+                } else {
+                    this.$emit('input', false);
+                }
+
             },
             clearCounter() {
                 if (this.countDownTimerId) {
@@ -92,30 +93,28 @@
                 }
             },
             showChanged() {
+                // Reset counter status
+                this.clearCounter();
                 // Reset dismiss status
                 this.dismissed = false;
 
                 // No timer for boolean values
                 if (this.show === true || this.show === false || this.show === null || this.show === 0) {
-                    this.localDismissible = this.dismissible;
                     return;
                 }
 
-                // Hide dismiss button for auto-dismissing
-                this.localDismissible = false;
-
                 // Start counter
-                this.clearCounter();
                 let dismissCountDown = this.show;
                 this.$emit('dismiss-count-down', dismissCountDown);
                 this.countDownTimerId = setInterval(() => {
+                    if (dismissCountDown < 1) {
+                        this.dismiss;
+                        return;
+                    }
                     dismissCountDown--;
                     this.$emit('dismiss-count-down', dismissCountDown);
-                    if (dismissCountDown < 1) {
-                        this.dismiss();
-                        this.clearCounter();
-                    }
-                }, 1000);
+                    this.$emit('input', dismissCountDown);
+                 }, 1000);
             }
         }
     };

commit 0752044a656d6f7e1175e93c0e49acd068632024

fix(alert) dismiss event broken after PR #897

lib/components/alert.vue
@@ -105,10 +105,9 @@
 
                 // Start counter
                 let dismissCountDown = this.show;
-                this.$emit('dismiss-count-down', dismissCountDown);
                 this.countDownTimerId = setInterval(() => {
                     if (dismissCountDown < 1) {
-                        this.dismiss;
+                        this.dismiss();
                         return;
                     }
                     dismissCountDown--;